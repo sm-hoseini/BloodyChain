3cb1b0c29b8d0230a68f1f3f700adc1f84bd6b4d
Sayed Mohammad Hoseini <mr.smhoseini@gmail.com>
2018-01-27 16:54:27 +0330

    adding Light

---
+++ Assets/Scripts/VFX/torchLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torchLight : MonoBehaviour {
+    Light light;
+   public  float minIntensity = 30, rangeIntensity = 20,minrange, rangeRange;
+	// Use this for initialization
+	void Start () {
+        light=this.GetComponent<Light>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        light.intensity =minIntensity+rangeIntensity* Mathf.PerlinNoise(Time.time,0);
+        light.spotAngle=minrange + Mathf.PerlinNoise(Time.time, 0) *rangeRange;
+    }
+}
