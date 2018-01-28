d7a470758fa3a00d62eb3ac8d23b8aba8875b29b
Amirreza <am.anvari@gmail.com>
2018-01-28 14:45:21 +0330

    menu

--- Assets/Scripts/MainMenu1.cs
+++ Assets/Scripts/MainMenu1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu1 : MonoBehaviour {
-
+    public GameObject gameObjectLoading;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,6 +15,8 @@ public class MainMenu1 : MonoBehaviour {
 	}
     public void BtnStart()
     {
+        Debug.Log("start");
+        gameObjectLoading.SetActive(true);
         Application.LoadLevel("GamePlayPrototype");
     }
 }
